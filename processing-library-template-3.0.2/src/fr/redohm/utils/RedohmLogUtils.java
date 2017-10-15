c2f9c0b44d7d3c096eefb4b3d5637ad80816f07a
Bertrand BENOIT <redohm@bertrand-benoit.net>
2017-10-15 18:55:15 +0200

    Implemented light Redohm Log Utils class allowing in future to switch to another Log System.

---
+++ processing-library-template-3.0.2/src/fr/redohm/utils/RedohmLogUtils.java
@@ -0,0 +1,30 @@
+package fr.redohm.utils;
+
+
+/**
+ * Utility methods to log message, warning and error.
+ * 
+ * @author <mailto:redohm@bertrand-benoit.net>Bertrand BENOIT</mailto>
+ *
+ */
+public final class RedohmLogUtils {
+	
+	// TODO: uses a logger system like the Apache one.
+
+	public static final void logMessage(final String message) {
+		System.out.println(message);
+	}
+
+	public static final void logWarning(final String message) {
+		System.err.println("[WARNING] " + message);
+	}
+
+	public static final void logError(final String message) {
+		System.err.println("[ERROR] " + message);
+	}
+
+	public static final void logException(final String message, final Exception e) {
+		logError(message + ": " + e.toString());
+	}
+
+}
